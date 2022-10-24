27d41e17e8d36aa049391ae658c962d43ee9ec9f
panjiyudasetya <panjiyudasetya@gmail.com>
2022-10-24 17:04:58 +0700

    Clean up `UserView`

--- simple_web_app/src/components/UserView.vue
+++ simple_web_app/src/components/UserView.vue
@@ -1,6 +1,5 @@
 <script>
 import axios from 'axios';
-import xml2js from 'xml2js';
 
 export default {
   name: 'UserView',
@@ -11,73 +10,22 @@ export default {
     }
   },
   mounted() {
-    // Setting `isRss` parameter to `false` for rendering JSON data.
-    // this.loadUsers(
-    //   "http://0.0.0.0:1234/api/users/",
-    //   false
-    // )
-
-    // Setting `isRss` parameter to `true` for rendering XML data.
-    this.loadUsers(
-      "http://0.0.0.0:1234/rss/users/",
-      true
-    )
+    this.loadUsers()
   },
   methods: {
-    loadUsers(url, isRss) {
-      this.content = null;
+    loadUsers() {
+      const url = "http://0.0.0.0:1234/api/users/"
       this.loading = true;
 
       axios.get(url).then(response => {
-        if (isRss) {
-          console.log("Parsing RSS ... ")
-          this.parseRSS(response.data).then(result => {
-            this.users = result
-            this.loading = false;
-          })
-        } else {
-          console.log("Asigning JSON data to the component's state ... ", response.data)
-          this.users = response.data;
-          this.loading = false;
-        }
+        console.log("Asigning JSON data to the component's state ... ", response.data)
+        this.users = response.data;
+        this.loading = false;
       }).catch((reason) => {
         this.loading = false;
         console.error(reason)
       });
     },
-    parseRSS(rssUser) {
-      return new Promise(resolve => {
-        let tag ="";
-        let to_return = [];
-
-        let parser = new xml2js.Parser({
-          trim: true,
-          explicitArray: true
-        });
-
-        parser.parseString(rssUser, function (err, result) {
-          if (err) {
-            console.error(err)
-          }
-          console.log(result);
-          let root = result.users;
-
-          for (tag in root.user) {
-            let item = root.user[tag];
-
-            to_return.push({
-              identifier: item.identifier[0],
-              first_name: item.first_name[0],
-              last_name: item.last_name[0],
-              email: item.email[0],
-              is_active: item.is_active[0],
-            });
-          }
-
-          resolve(to_return);  
-        });
-      })
-    }
   }
 };
 </script>
@@ -88,6 +36,7 @@ export default {
     <table>
       <thead>
         <td>ID</td>
+        <td>Username</td>
         <td>First Name</td>
         <td>Last Name</td>
         <td>Email</td>
@@ -95,8 +44,9 @@ export default {
       </thead>
       <tbody>
         <tr v-if="loading">Load list users...</tr>
-        <tr v-for="user in users" :key="user.identifier">
-          <td>{{ user.identifier }}</td>
+        <tr v-for="user in users" :key="user.id">
+          <td>{{ user.id }}</td>
+          <td>{{ user.username }}</td>
           <td>{{ user.first_name }}</td>
           <td>{{ user.last_name }}</td>
           <td>{{ user.email }}</td>
